d0bcd7623f9f8745ce5e123db1329d5211c0256e
Timoteo Rider Sweeney <timoteo2.25@gmail.com>
2025-11-30 23:44:49 -0600

    Add basic insert to cvec

--- src/structs/cvec.rs
+++ src/structs/cvec.rs
@@ -20,6 +20,37 @@ impl<T> CVec<T> {
         }
     }
 
+    pub fn insert(&mut self, item: T) {
+        unsafe {
+            if self.items.is_null() {
+                // Allocate initial space for one item
+                let new_items = libc::malloc(std::mem::size_of::<T>()) as *mut T;
+                if new_items.is_null() {
+                    panic!("Failed to allocate memory for CVec");
+                }
+                self.items = new_items;
+                self.items_end = new_items;
+                self.items_rsv_end = new_items.add(1);
+            } else if self.items_end == self.items_rsv_end {
+                // Need to reallocate
+                let current_len = self.len();
+                let new_capacity = if current_len == 0 { 1 } else { current_len * 2 };
+                let new_size = new_capacity * std::mem::size_of::<T>();
+                let new_items = libc::realloc(self.items as *mut libc::c_void, new_size) as *mut T;
+                if new_items.is_null() {
+                    panic!("Failed to reallocate memory for CVec");
+                }
+                self.items = new_items;
+                self.items_end = new_items.add(current_len);
+                self.items_rsv_end = new_items.add(new_capacity);
+            }
+
+            // Insert the new item
+            std::ptr::write(self.items_end as *mut T, item);
+            self.items_end = self.items_end.add(1);
+        }
+    }
+
     /// Returns the number of items in the CVec.
     pub fn len(&self) -> usize {
         if self.items.is_null() || self.items_end.is_null() {
